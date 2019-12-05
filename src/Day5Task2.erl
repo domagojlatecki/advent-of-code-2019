10f05ecb74d0e5190dd31dc702c44ae4bdcf7fc6
Domagoj Latečki <domagoj.latecki@gmail.com>
2019-12-05 22:51:17 +0100

    Day 5 - Task 2

--- src/Day5Task2.erl
+++ src/Day5Task2.erl
@@ -1,4 +1,4 @@
--module('Day5Task1').
+-module('Day5Task2').
 -export([main/0]).
 
 threeArgOp(Mode, PC, Memory) ->
@@ -35,7 +35,7 @@ mulOp(Mode, PC, Memory) ->
 
 readOp(PC, Memory) ->
     Dest = array:get(PC + 1, Memory),
-    Value = 1,
+    Value = 5,
     NewMemory = array:set(Dest, Value, Memory),
     {NewMemory, PC + 2}.
 
@@ -54,6 +54,65 @@ writeOp(Mode, PC, Memory) ->
             {Memory, PC + 2}
     end.
 
+twoArgOp(Mode, PC, Memory) ->
+    A = array:get(PC + 1, Memory),
+    B = array:get(PC + 2, Memory),
+    ModeA = Mode rem 10,
+    ModeB = Mode div 10,
+    Cond = if
+        ModeA == 1 ->
+            A;
+        true ->
+            array:get(A, Memory)
+    end,
+    Loc = if
+        ModeB == 1 ->
+            B;
+        true ->
+            array:get(B, Memory)
+    end,
+    {Cond, Loc}.
+
+jumpIfTrueOp(Mode, PC, Memory) ->
+    {Cond, Loc} = twoArgOp(Mode, PC, Memory),
+    if
+        Cond /= 0 ->
+            {Memory, Loc};
+        true ->
+            {Memory, PC + 3}
+    end.
+
+jumpIfFalseOp(Mode, PC, Memory) ->
+    {Cond, Loc} = twoArgOp(Mode, PC, Memory),
+    if
+        Cond == 0 ->
+            {Memory, Loc};
+        true ->
+            {Memory, PC + 3}
+    end.
+
+ltOp(Mode, PC, Memory) ->
+    {C, ValueA, ValueB} = threeArgOp(Mode, PC, Memory),
+    ValueC = if
+        ValueA < ValueB ->
+            1;
+        true ->
+            0
+    end,
+    NewMemory = array:set(C, ValueC, Memory),
+    {NewMemory, PC + 4}.
+
+eqOp(Mode, PC, Memory) ->
+    {C, ValueA, ValueB} = threeArgOp(Mode, PC, Memory),
+    ValueC = if
+        ValueA == ValueB ->
+            1;
+        true ->
+            0
+    end,
+    NewMemory = array:set(C, ValueC, Memory),
+    {NewMemory, PC + 4}.
+
 execProgram(PC, Memory) ->
     Value = array:get(PC, Memory),
     OpCode = Value rem 100,
@@ -70,6 +129,18 @@ execProgram(PC, Memory) ->
             execProgram(NewPC, NewMemory);
         OpCode == 4 ->
             {NewMemory, NewPC} = writeOp(Mode, PC, Memory),
+            execProgram(NewPC, NewMemory);
+        OpCode == 5 ->
+            {NewMemory, NewPC} = jumpIfTrueOp(Mode, PC, Memory),
+            execProgram(NewPC, NewMemory);
+        OpCode == 6 ->
+            {NewMemory, NewPC} = jumpIfFalseOp(Mode, PC, Memory),
+            execProgram(NewPC, NewMemory);
+        OpCode == 7 ->
+            {NewMemory, NewPC} = ltOp(Mode, PC, Memory),
+            execProgram(NewPC, NewMemory);
+        OpCode == 8 ->
+            {NewMemory, NewPC} = eqOp(Mode, PC, Memory),
             execProgram(NewPC, NewMemory);
         OpCode == 99 ->
             unit;
